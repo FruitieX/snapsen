2a69f83b5967745c0074fa674fefd84ac2af291d
Rasmus Eskola <fruitiex@gmail.com>
2017-03-18 02:15:45 +0200

    Disable loggerMiddleware to improve performance

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,11 +1,11 @@
 
 import promiseMiddleware from 'redux-promise';
 import thunkMiddleware from 'redux-thunk';
-import loggerMiddleware from './middleware/loggerMiddleware';
+//import loggerMiddleware from './middleware/loggerMiddleware';
 
 // define store middlewares as an array
 export default [
   promiseMiddleware,
   thunkMiddleware,
-  loggerMiddleware
+  //loggerMiddleware
 ];
