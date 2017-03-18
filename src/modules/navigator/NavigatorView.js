65483a4f31a4910d1194da7cf30d3466f1a691ff
Rasmus Eskola <fruitiex@gmail.com>
2017-03-18 02:58:00 +0200

    Revert "No longer need to connect NavigatorView to redux"
    This reverts commit f9243b1b2d4a6c75c4044e8913f76eebf3a3db86.

--- src/modules/navigator/NavigatorView.js
+++ src/modules/navigator/NavigatorView.js
@@ -1,9 +1,21 @@
-import React, {Component} from 'react';
+import React, {PropTypes, Component} from 'react';
+import {addNavigationHelpers} from 'react-navigation';
 
 import AppNavigator from './Navigator';
 
 class NavigatorView extends Component {
   static displayName = 'NavigationView';
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    navigatorState: PropTypes.shape({
+      index: PropTypes.number.isRequired,
+      routes: PropTypes.arrayOf(PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        routeName: PropTypes.string.isRequired
+      }))
+    }).isRequired
+  };
 
   render() {
     return (
