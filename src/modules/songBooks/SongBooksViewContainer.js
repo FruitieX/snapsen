91478be682be0f7e615877130e9f9736154f9fad
Rasmus Eskola <fruitiex@gmail.com>
2017-04-05 14:45:37 +0300

    Fix search with new books state

--- src/modules/songBooks/SongBooksViewContainer.js
+++ src/modules/songBooks/SongBooksViewContainer.js
@@ -1,20 +1,8 @@
 import {connect} from 'react-redux';
 import SongBooksView from './SongBooksView';
-import debounce from 'lodash/throttle';
-import {clearSearch, searchChange} from '../../actions/appActions';
-
-import rest from '../../utils/rest';
-
-const refresh = (dispatch, filter) => dispatch(rest.actions.songs({filter}));
 
 export default connect(
   state => ({
     books: state.books
-  }),
-  dispatch => ({
-    refresh: (filter) => refresh(dispatch, filter),
-    changeSearch: (text) => dispatch(searchChange(text)),
-    clearSearch: () => dispatch(clearSearch()),
-    throttledRefresh: debounce((filter) => refresh(dispatch, filter), 500, {leading: true})
   })
 )(SongBooksView);
