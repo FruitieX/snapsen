8273b4bd87c6a37c7b0abe18da56b294c86cfca1
Rasmus Eskola <fruitiex@gmail.com>
2017-08-29 21:49:57 +0300

    Remove Pepperoni logo

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,28 +1,10 @@
 import { Platform, StatusBar } from 'react-native';
 import styled from 'styled-components/native';
-import PepperoniLogoAsset from '../../assets/pepperoni-logo.png';
-import PepperoniIconAsset from '../../assets/pepperoni-icon.png';
 
 export const Centered = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-`;
-
-export const PepperoniLogo = styled.Image.attrs({
-  source: PepperoniLogoAsset,
-  resizeMode: 'contain',
-})`
-  align-self: center;
-  width: 80%;
-`;
-
-export const PepperoniIcon = styled.Image.attrs({
-  source: PepperoniIconAsset,
-  resizeMode: 'contain',
-})`
-  flex: 1;
-  width: 70px;
 `;
 
 export const Bold = styled.Text`font-weight: bold;`;
